442b92c9474e539aa8835ad44a9e85f11e5bb96c
shatse <shannon.x.tse@gmail.com>
2019-04-25 23:25:05 -0700

    more changes

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # liri-node-app
+
+### Overview
+
+This assignment is called Liri. Like Apple's Siri, Liri is a language interpretation and recognition interface. Liri will take in a request and give you back whatever you search for, in our case being a song from Spotify, concerts from Bands in Town, and movies from OMDB. 
+
+### Examples
+
+In your terminal, you will have four options to choose from: 
+- concert-this
+- spotify-this-song
+- movie-this
+- doWhatItSays
+
+Depending on which one you type in, they should show your search items as follows.
+
+1. For concerts:
+
+
+
+
+
+2. For a Spotify song:
+
+
+
+
+
+3. For a movie:
+
--- liri.js
+++ liri.js
@@ -1,5 +1,29 @@
 require("dotenv").config();
 
 var keys = require("./keys.js");
+var spotify = new Spotify(keys.spotify);
+
+var searchItem = process.argv[2];
+var searchResult = process.argv.slice(3).join(" ");
+
+const fs = require('fs');
+const axios = require('axios');
+const moment = require('moment');
+const Spotify = require('node-spotify-api');
+
+switch (searchItem) {
+    case "concert-this":
+        console.log('concert-this');
+        break;
+    case "spotify-this-song":
+        console.log('spotify-this-song');
+        break;
+    case "movie-this":
+        console.log('movie-this');
+        break;
+    case "do-what-it-says":
+        console.log('do-what-it-says');
+        break;
+}
 
 
